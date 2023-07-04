f7508b167824877b20c54b78070455c72da637e1
Mohit <mohit.kumar@vcareall.com>
2023-07-04 22:29:14 +0100

    test

--- app/src/main/java/com/e/testrim/ui/MainActivity.kt
+++ app/src/main/java/com/e/testrim/ui/MainActivity.kt
@@ -48,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         getStationData()
         buttonRefresh.setOnClickListener {
             stationDataRepository.getStationData(stationName)
+            //error fixed
             getStationData()
         }
     }
